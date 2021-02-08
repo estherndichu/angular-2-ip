6ab6e61a19f4c22b3866e109414caf57ef4b7c25
estherndichu <itskuijenga@gmail.com>
2021-02-08 05:29:47 +0300

    generate user-http service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  personalAccessToken : 'de7f4e49cc8ca77423ca24a0b7f7afa65da058f3'
 };
 
 /*
