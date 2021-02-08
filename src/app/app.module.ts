788d4906bfc99f871a4c0dc786944ebf32c79051
estherndichu <itskuijenga@gmail.com>
2021-02-08 07:32:29 +0300

    place access token in environment folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserFormComponent
+    UserFormComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
